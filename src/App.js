04e907e5f6272229316fd8a068962e30c0d0ab13
Arsh10x <arshad.a0921@gmail.com>
2024-03-27 23:52:15 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CartItems from "./components/Cart/Cart";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Form from "./components/Form/Form";
+import ErrorPage from "./ErrorPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route path="/cartItem" element={<CartItems />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
